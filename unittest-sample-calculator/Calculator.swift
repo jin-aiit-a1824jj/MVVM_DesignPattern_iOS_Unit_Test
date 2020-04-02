ecf0f334c170aaf8f57dd9ab69ec11793bfda156
a1824jj <a1824jj@aiit.ac.jp>
2020-04-02 17:49:54 +0900

    sample test

---
+++ unittest-sample-calculator/Calculator.swift
@@ -0,0 +1,21 @@
+//
+//  Calculator.swift
+//  unittest-sample-calculator
+//
+//  Created by JONGWOO JIN on 2020/04/02.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    
+    func add(_ a: Int, _  b: Int) -> Int{
+        return a+b
+    }
+    
+    func sub(_ a: Int, _  b: Int) -> Int{
+        return a-b
+    }
+    
+}
--- unittest-sample-calculatorTests/unittest_sample_calculatorTests.swift
+++ unittest-sample-calculatorTests/unittest_sample_calculatorTests.swift
@@ -7,27 +7,24 @@
 //
 
 import XCTest
+@testable import unittest_sample_calculator
 
 class unittest_sample_calculatorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_AddTwoNumbers() {
+        
+        let calculator = Calculator()
+        let result = calculator.add(2,3)
+        
+        XCTAssertEqual(result, 9)
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_SubtractTwoNumbers() {
+        let calculator = Calculator()
+        let result = calculator.sub(2,3)
+               
+        XCTAssertEqual(result, -1)
     }
 
 }
